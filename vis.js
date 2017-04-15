c833a5eab055a3117e92451b8f7d9e969265a95c
Jeannelle Alford <jkalfor2@illinois.edu>
2017-04-14 23:04:04 -0500

    Add JS file skeleton

---
+++ vis.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/* Boilerplate jQuery */
+$(function() {
+  $.get("res/fileName.csv")
+   .done(function (csvData) {
+     var data = d3.parseCsv(csvData);
+     visualize(data);
+   })
+  .fail(function(e) {
+     alert("Failed to load CSV file!");
+  });
+});
+
+/* Visualize the data in the visualize function */
+var visualize = function(data) {
+  console.log(data);
+
+  // == BOILERPLATE ==
+  var margin = { top: 50, right: 50, bottom: 50, left: 50 },
+     width = 800 - margin.left - margin.right,
+     height = (data.length * 20);
+
+  var svg = d3.select("#chart")
+              .append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+              .style("width", width + margin.left + margin.right)
+              .style("height", height + margin.top + margin.bottom)
+              .append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+  // == Your code! :) ==
+
+};
