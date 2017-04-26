332d6eda622f5ef647e17e965b637ba4073f3495
Diana Kowalska <diana.anna.kowalski@gmail.com>
2017-04-25 20:14:43 -0500

    resizing works when you reload the page

--- vis.js
+++ vis.js
@@ -1,135 +1,219 @@
 "use strict";
 
+/*
+in real life this is a bad idea. but we're kinda limited here, because otherwise
+this seems to produce a lot of scoping errors that show up as "unexpected NaN"
+*/
+var data;
+
 /* Boilerplate jQuery */
-$(function() {
+$(function () {
 	$.get("res/uiuc_demographics_2005_15.csv")
-	.done(function (csvData) {
-		var data = d3.csvParse(csvData);
-		visualize(data);
-	})
-	.fail(function(e) {
-		alert("Failed to load CSV file!");
-	});
+		.done(function (csvData) {
+			data = d3.csvParse(csvData);
+			visualize(data);
+		})
+		.fail(function (e) {
+			alert("Failed to load CSV file!");
+		});
 });
 
 /* Visualize the data in the visualize function */
-var visualize = function(data) {
-// 	console.log(data);
-	
- 	data = data.filter(function(d) {
-			return d["Fall"] == 2015; // probably want to change this at some point
-			/*
-			Don't use triple equals here, because we don't import the year as a number
-			also, "Fall" => "year", but the CSV is formatted oddly.
-			 */
-		})
-	
+var visualize = function (data) {
+	// 	console.log(data);
+
+	data = data.filter(function (d) {
+		return d["Fall"] == "2015"; // probably want to change this at some point
+		/*
+		Don't use triple equals here, because we don't import the year as a number
+		also, "Fall" => "year", but the CSV is formatted oddly.
+		 */
+	});
+
+
 	// == BOILERPLATE ==
-	var margin = { top: 50, right: 50, bottom: 50, left: 50 },
-		width = 800 - margin.left - margin.right,
-		height = (data.length * 10); // TODO fix
+	var margin = 20,
+		width = window.innerWidth - 2 * margin,
+		height = window.innerHeight - 2 * margin;
+	// designed for 1920x1080...
+	//not anymore :)
+
+	var minHW = Math.min(window.innerWidth, window.innerHeight);
+
 
 	var svg = d3.select("#chart")
-				.append("svg")
-				.attr("width", width + margin.left + margin.right)
-				.attr("height", height + margin.top + margin.bottom)
-				.style("width", width + margin.left + margin.right)
-				.style("height", height + margin.top + margin.bottom)
-				.append("g")
-				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		.append("svg")
+		.attr("width", width + 2 * margin)
+		.attr("height", height + 2 * margin)
+		.style("width", width + 2 * margin)
+		.style("height", height + 2 * margin)
+		.append("g")
+		.attr("transform", "translate(" + margin + "," + margin + ")");
+
+	var padding = 1.2;
 
 	/*
-		The data right now has a circle for every single line on the CSV, which
-		has multiple of the same major. They need to be combined (using pandas? idk)
-		so that there is only one circle per major (not forgetting to add together
-		the values of all the duplicate rows). That way there will only be one
-		circle per major. -- I think someone was working on this
+	https://bl.ocks.org/mbostock/7881887
+	https://bl.ocks.org/shancarter/f621ac5d93498aa1223d8d20e5d3a0f4
+	https://bl.ocks.org/ericsoco/d2d49d95d2f75552ac64f0125440b35e
+	or just google d3 clustered force layout
 	*/
 
-	var padding = 2, // separation between same-color nodes
-		clusterPadding = 4; // separation between different-color nodes
-	
-	// https://bl.ocks.org/mbostock/7881887
-	// or just google d3 clustered force layout
-	
-	// Haven't done anything with the majors and colleges vars yet.
-	// I think they'll come in handy sometime. -Jeannelle
+	// this one is unused
 	var majors = _.map(data, "Major Code");
 	majors = _.uniq(majors);
 
 	var colleges = _.map(data, "College");
 	colleges = _.uniq(colleges);
 
-	// test variable to allow us to ttach stuff to the bottom
-	var yValue = 10;
-	
-	
 	// tooltip code for ease of use
 	var tip = d3.tip()
-			.attr('class', 'd3-tip')
-			.html(function(d) {
-				return d["Fall"] + " - " + d["Major Name"];
-			});
+		.attr('class', 'd3-tip')
+		.html(function (d) {
+			var major = d["Major Name"];
+			var data = d["Fall"] + " - " + d["College"] + "/" + major + "<br />";
+
+			// plurals are important
+			var students = parseInt(d["Total"]);
+			if (students == 1)
+				data += "Only one student"; // how lonely
+			else
+				data += students + " students";
+
+			// just for fun
+			if (_.includes(major, "Computer Science"))
+				data += " who can make more visualizations like this!";
+			else if (_.includes(major, "Crop Science"))
+				data += " in the <em>other</em> CS";
+
+			return data;
+		});
 	svg.call(tip)
-	
+
 	// color scale??? can we reconcile it with the below?
-	var color = d3.scaleOrdinal(d3.schemeCategory20);
-		
-	// The colors are arbitrary.
-	var collegeColor = function(college){
-		if(college=="KP "){ //ENG
-		return "rgb(255, 127, 0)";
-		} else if(college=="KM "){ //MUS
-		return "rgb(24, 9, 91)";
-		} else if(college=="KT "){ //COM
-		return "rgb(42, 119, 21)";
-		// TODO: LE LM
-		} else if(college=="KV "){ //LAS
-		return "rgb(158, 23, 70)";
-		} else if(college=="LC "){ //Vet Med // why is Vet Med "LC" ??????
-		return "158, 23, 70)";
-		} else if(college=="KY "){ //AHS
-		return "red";
-		} else if(college=="KL "){ //ACES
-		return "blue";
-		} else if(college=="KN "){ //EDU
-		return "rgb(118, 6, 155)";
-		} else if(college=="KR "){ //FAA
-		return "rgb(158, 234, 25)";
-		} else if(college=="LG "){ //Labor
-		return "rgb(158, 234, 25)";
-		} else if(college=="KW "){ //DGS
-		return "rgb(66, 55, 43)";
-		} else if(college=="LL "){ //SSW
-		return "rgb(66, 55, 43)";
-		} else if(college=="KU "){ //Law
-		return "rgb(66, 55, 43)";
-		} else if(college=="LP "){ //LIS
-		return "rgb(61, 255, 206)";
-		} else if(college=="LE "){ //Aviation
-		return "rgb(255, 255, 61)"
-		} else{
-		return "black"
-		}
-	}
-	
-	// ACTUAL CODE THAT DOES STUFF WOW
-	svg.selectAll("circles")
-		.data(data)
-		.enter()
-		// everything below enter loops through once for every piece of data
-		.append("circle")
-		.attr("r", 5)
-		.attr("cx", 10)
-		// temporary function
-		.attr("cy", function(){
-			var ret = yValue;
-			yValue += 10;
-			return ret;
+	var color = d3.scaleSequential(d3.interpolateRainbow);
+
+	var clusters = new Array(colleges.length)
+	var maxRadius = 0;
+
+	// generate nodes
+	var nodes = data.map(function(d) {
+		var i = colleges.indexOf(d["College"]),
+			r = parseInt(d["Total"]) ** 0.5 * minHW/750 + 1;
+
+		d.cluster = i;
+		d.radius = r;
+		d.r = r + padding; // for circle packing
+
+		// numbers subject to change
+		var dist = Math.random() ** 2 * minHW/3 + Math.max((8 - r) * minHW/10, 0);
+		// spread out smaller ones to the outer edge
+		d.x = Math.cos((i + Math.random()) / colleges.length * 2 * Math.PI) * dist + width / 2 + Math.random();
+		d.y = Math.sin((i + Math.random()) / colleges.length * 2 * Math.PI) * dist + height / 2 + Math.random();
+
+		if (!clusters[i] || (r > clusters[i].radius)) clusters[i] = d;
+		if (maxRadius < d.radius) maxRadius = d.radius;
+		return d;
+	});
+
+	// HIGHLY DANGEROUS MAGIC
+	// d3.packSiblings(nodes);
+
+	var node = svg.selectAll('circle')
+		.data(nodes)
+		.enter().append('circle')
+		// styling
+		.style("fill", function (d) {
+			return color(d.cluster * 1.0 / clusters.length);
 		})
-		.attr("fill", function(d){
-			return color(d["College"]); // how do you do custom D3 color scales!??
+		.style("stroke", function (d) {
+			return d3.color(color(d.cluster)).darker(0.5);
 		})
+		.style("stroke-width", 1)
+		// positioning
+		.attr("r", function (d) {
+			return 0; // d.radius;
+		})
+		.attr("cx", (d) => d.x)
+		.attr("cy", (d) => d.y)
+		// tooltip
 		.on("mouseover", tip.show)
 		.on('mouseout', tip.hide)
+		// drag behavior
+		.call(d3.drag()
+			.on('start', dragstarted)
+			.on('drag', dragged)
+			.on('end', dragended)
+		);
+
+
+	// drag behavior implementation
+	function dragstarted (d) {
+		if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+		d.fx = d.x;
+		d.fy = d.y;
+	}
+
+	function dragged (d) {
+		d.fx = d3.event.x;
+		d.fy = d3.event.y;
+	}
+
+	function dragended (d) {
+		if (!d3.event.active) simulation.alphaTarget(0);
+		d.fx = null;
+		d.fy = null;
+	}
+
+	// The clustering currently works ok, but causes LAS to screw around and start doing backflips.
+	// strength parameters need tuning
+	var simulation = d3.forceSimulation()
+		// keep entire simulation balanced around screen center
+		.force('center', d3.forceCenter(width/2, height/2))
+
+		.force("gravity", d3.forceManyBody().strength(1))
+
+		// this one is not strictly necessary?
+		.force('attract', d3.forceAttract()
+			.target([width/2, height/2])
+			.strength(0.01))
+
+		.force('cluster', d3.forceCluster()
+			.centers(function (d) { return clusters[d.cluster]; })
+			.strength(0.5)
+			.centerInertia(0.1))
+
+		// apply collision with padding
+		.force('collide', d3.forceCollide(function (d) { return d.radius + padding; })
+			.strength(0))
+
+		.on('tick', layoutTick)
+		.nodes(nodes);
+
+	function layoutTick (e) {
+		node
+			.attr('cx', function (d) { return d.x; })
+			.attr('cy', function (d) { return d.y; })
+	}
+
+	// ramp up collision strength to provide smooth transition
+	var transitionTime = 2000;
+
+	var t = d3.timer(function (elapsed) {
+		var dt = elapsed / transitionTime;
+		simulation.force('collide').strength(Math.pow(dt, 2) * 1);
+		if (dt >= 1.0) t.stop();
+	});
+
+	// transition radii
+	node.transition()
+		.duration(transitionTime / 2)
+		.delay(function(d, i) { return i * 9; })
+		.attrTween("r", function(d) {
+			var i = d3.interpolate(0, d.radius);
+			return function(t) { return d.radius = i(t); };
+	});
+
+
+
 };
